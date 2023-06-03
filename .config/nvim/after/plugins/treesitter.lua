86453ca8f09faf4c6a05d0bb9e2400e1db20788b
Marco Santos <marconlsantos@yahoo.co.uk>
2023-06-03 23:41:16 +0100

    Split editor.lua into plugin files

---
+++ .config/nvim/after/plugins/treesitter.lua
@@ -0,0 +1,11 @@
+require("nvim-treesitter.configs").setup({
+	ensure_installed = { "help", "javascript", "typescript", "lua", "vim", "c_sharp" },
+	auto_install = true,
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
+	indent = {
+		enable = true,
+	},
+})
