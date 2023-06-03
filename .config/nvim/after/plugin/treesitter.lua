6155cbbc007bc82e96deb8dd5106c4c94f045ff0
Marco Santos <marconlsantos@yahoo.co.uk>
2023-06-03 23:50:19 +0100

    Correct folder name

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require("nvim-treesitter.configs").setup({
-	ensure_installed = { "help", "javascript", "typescript", "lua", "vim", "c_sharp" },
+	ensure_installed = { "javascript", "typescript", "lua", "vim", "c_sharp" },
 	auto_install = true,
 	highlight = {
 		enable = true,
