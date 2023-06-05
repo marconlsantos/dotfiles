f53d4063d844eb930ab08cd43c83f10cfbe018af
Marco Santos <marconlsantos@yahoo.co.uk>
2023-06-05 21:04:22 +0100

    Split LSP installation and config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,105 +1,6 @@
-local signs = {
-	{ name = "DiagnosticSignError", text = "" },
-	{ name = "DiagnosticSignWarn", text = "" },
-	{ name = "DiagnosticSignHint", text = "" },
-	{ name = "DiagnosticSignInfo", text = "" },
-}
-
-for _, sign in ipairs(signs) do
-	vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-end
-
-local config = {
-	signs = {
-		active = signs,
-	},
-	update_in_insert = true,
-	underline = true,
-	severity_sort = true,
-	float = {
-		focusable = false,
-		style = "minimal",
-		border = "rounded",
-		source = "always",
-		header = "",
-		prefix = "",
-	},
-}
-
-vim.diagnostic.config(config)
-
-local servers = {
-	"lua_ls",
-	"tsserver",
-	"omnisharp",
-}
-
 return {
 	{
 		"neovim/nvim-lspconfig",
-		config = function()
-			-- Setup language servers.
-			local lspconfig = require("lspconfig")
-
-			local server_opts = {}
-
-			local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-			if status_ok then
-				server_opts.capabilities = cmp_nvim_lsp.default_capabilities()
-			end
-
-			for _, value in pairs(servers) do
-				local require_ok, conf_opts = pcall(require, "plugins.settings." .. value)
-
-				if require_ok then
-					server_opts = vim.tbl_deep_extend("force", conf_opts, server_opts)
-				end
-
-				lspconfig[value].setup(server_opts)
-			end
-
-			-- Global mappings.
-			-- See `:help vim.diagnostic.*` for documentation on any of the below functions
-			vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float)
-			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-			vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-			vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
-
-			-- Use LspAttach autocommand to only map the following keys
-			-- after the language server attaches to the current buffer
-			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-				callback = function(ev)
-					-- Enable completion triggered by <c-x><c-o>
-					--vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
-
-					-- Buffer local mappings.
-					-- See `:help vim.lsp.*` for documentation on any of the below functions
-					local opts = { buffer = ev.buf }
-
-					vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
-
-					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-					vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-
-					vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
-					vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
-					vim.keymap.set("n", "<leader>wl", function()
-						print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-					end, opts)
-
-					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-					vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-					vim.keymap.set("n", "<leader>fm", function()
-						vim.lsp.buf.format({ async = true })
-					end, opts)
-				end,
-			})
-		end,
 	},
 	{
 		"williamboman/mason.nvim",
@@ -108,16 +9,5 @@ return {
 			"neovim/nvim-lspconfig",
 		},
 		build = ":MasonUpdate",
-		keys = {
-			{ "<leader>m", "<cmd>Mason<cr>", desc = "Mason" },
-		},
-		init = function()
-			require("mason").setup()
-
-			require("mason-lspconfig").setup({
-				ensure_installed = servers,
-				automatic_installation = true,
-			})
-		end,
 	},
 }
