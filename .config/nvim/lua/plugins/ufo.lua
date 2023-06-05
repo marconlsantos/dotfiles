e2e9829b9996fbb7ef95e894585a18ce6eb8960b
Marco Santos <marconlsantos@yahoo.co.uk>
2023-06-05 21:55:50 +0100

    Consolidate UFO files

--- .config/nvim/lua/plugins/ufo.lua
+++ .config/nvim/lua/plugins/ufo.lua
@@ -1,4 +1,10 @@
 return {
 	"kevinhwang91/nvim-ufo",
 	dependencies = "kevinhwang91/promise-async",
+	config = true,
+	opts = {
+		provider_selector = function(bufnr, filetype, buftype)
+			return { "treesitter", "indent" }
+		end,
+	},
 }
