ea7aa12cd2a21978560f68260ddbec70ce25233f
Marco Santos <marconlsantos@yahoo.co.uk>
2024-05-23 21:27:56 +0100

    Telescope updates

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,12 +1,11 @@
-require("telescope").load_extension("fzf")
+require('telescope').load_extension('fzf')
 
-local tlscp = require("telescope.builtin")
+local tlscp = require('telescope.builtin')
 
 -- File pickers
-vim.keymap.set("n", "<leader>gf", tlscp.git_files, { desc = "Find Git files" })
-vim.keymap.set("n", "<leader>ff", tlscp.find_files, { desc = "Find files" })
-vim.keymap.set("n", "<leader>gr", tlscp.live_grep, { desc = "Live grep search" })
+vim.keymap.set('n', '<leader>ff', tlscp.find_files, { desc = 'Find files' })
+vim.keymap.set('n', '<leader>fg', tlscp.live_grep, { desc = 'Live grep search' })
 
 -- Vim pickers
-vim.keymap.set("n", "<leader>bf", tlscp.buffers, { desc = "List open buffers" })
-vim.keymap.set("n", "<leader>km", tlscp.keymaps, { desc = "Show keymaps" })
+vim.keymap.set('n', '<leader>vk', tlscp.keymaps, { desc = 'Show keymaps' })
+vim.keymap.set('n', '<leader>vh', tlscp.help_tags, { desc = 'Help tags' })
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,25 +1,21 @@
 return {
 	{
-		"nvim-telescope/telescope-fzf-native.nvim",
-		build = "make",
+		'nvim-telescope/telescope-fzf-native.nvim',
+		build = 'make',
 	},
 	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.1",
-		dependencies = { "nvim-lua/plenary.nvim" },
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.1',
+		dependencies = { 
+			'nvim-lua/plenary.nvim' ,
+			'BurntSushi/ripgrep',
+			'sharkdp/fd'
+		},
 		config = true,
 		opts = {
 			defaults = {
-				layout_config = { prompt_position = "top" },
-				sorting_strategy = "ascending",
-			},
-			pickers = {
-				diagnostics = {
-					theme = "dropdown",
-				},
-				lsp_document_symbols = {
-					theme = "dropdown",
-				},
+				layout_config = { prompt_position = 'top' },
+				sorting_strategy = 'ascending',
 			},
 		},
 	},
