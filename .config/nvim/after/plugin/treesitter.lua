86e48f623baeadf57978398b6eddd9da0d223e98
Marco Santos <marconlsantos@yahoo.co.uk>
2024-05-23 21:53:00 +0100

    Treesitter updates

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,5 +1,5 @@
-require("nvim-treesitter.configs").setup({
-	ensure_installed = { "javascript", "typescript", "lua", "vim", "c_sharp" },
+require('nvim-treesitter.configs').setup({
+	ensure_installed = { 'lua', 'vim', 'vimdoc', 'c_sharp', 'javascript', 'typescript', 'html', 'json5', 'xml' },
 	auto_install = true,
 	highlight = {
 		enable = true,
