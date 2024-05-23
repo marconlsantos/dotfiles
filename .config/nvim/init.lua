2914d457d2911e7892d56ea6d54717ab8bf400ee
Marco Santos <marconlsantos@yahoo.co.uk>
2024-05-23 21:55:34 +0100

    Remove call to deleted script

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,28 +5,26 @@ vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_ruby_provider = false
 vim.g.loaded_perl_provider = false
 
-require("options")
+require('options')
 
-require("keymaps")
+require('keymaps')
 
 -- use laz.nvim for module manegement
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
+		'git',
+		'clone',
+		'--filter=blob:none',
+		'https://github.com/folke/lazy.nvim.git',
+		'--branch=stable', -- latest stable release
 		lazypath,
 	})
 end
 
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require('lazy').setup('plugins')
 
-vim.cmd("colorscheme nightfox")
-
-require("cmds")
+vim.cmd('colorscheme nightfox')
