75fef0a14a68d3ad7aca9e1e11fe4bba590b2fef
Marco Santos <marconlsantos@yahoo.co.uk>
2024-06-01 13:04:43 +0100

    Color scheme config update

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,5 +1,12 @@
 return {
-	{ "rebelot/kanagawa.nvim", name = "kanagawa", priority = 1000 },
+	{
+		"rebelot/kanagawa.nvim",
+		name = "kanagawa",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("kanagawa")
+		end,
+	},
 	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
 	"numToStr/Comment.nvim",
 	{
