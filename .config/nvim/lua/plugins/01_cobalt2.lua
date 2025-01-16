741686beb41c2b0c778902490629c1bba658d2ff
Marco Santos <marconlsantos@outlook.pt>
2025-01-16 21:35:07 +0000

    Update theme

--- .config/nvim/lua/plugins/01_cobalt2.lua
+++
@@ -1,4 +0,0 @@
-return { 
-	'GlennLeo/cobalt2', 
-	config = function() vim.cmd.colorscheme 'cobalt2' end
-}
---
+++ .config/nvim/lua/plugins/01_theme.lua
@@ -0,0 +1,4 @@
+return {
+    'EdenEast/nightfox.nvim',
+    config = function() vim.cmd.colorscheme 'nightfox' end
+}
