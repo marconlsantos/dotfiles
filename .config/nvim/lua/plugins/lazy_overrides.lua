4656921c4d0ba8bfcf99ea92abdd0c26eec8df20
Marco Santos <marconlsantos@outlook.pt>
2025-10-28 21:23:13 +0000

    Lazy lsp override

---
+++ .config/nvim/lua/plugins/lazy_overrides.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      sections = {
+        lualine_z = {},
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = {
+        enabled = false,
+      },
+    },
+  },
+}
